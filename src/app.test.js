01d06d62797c8d45a798bdc41fe7b61ed72d9f5c
Cleop <cleo.pearson@hotmail.co.uk>
2017-01-27 10:57:56 +0000

    Tests the index.html route is working

--- src/app.test.js
+++ src/app.test.js
@@ -5,8 +5,19 @@ it('checks the server is running', (t) => {
   server.start((err) => {
     if (err) {
       t.error(err);
-    } else { t.pass('Server is running'); }
+    } else { t.pass('PASS: Server is running'); }
     server.stop();
     t.end();
+  });
+});
+
+it(`checks the '/' route`, (t) => {
+  const options = {
+    method: 'GET',
+    url:'/',
+  };
+  server.inject(options, (response) => {
+    t.equal(response.statusCode, 200, `PASS: You received a 200 status code for '/' route`);
+    t.end()
   });
 });
